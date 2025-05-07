01b8f13a6bc629a8957c3f5d0e5470a3584f68ed
farazali110 <farazmahar512@gmail.com>
2025-05-07 15:57:54 +0500

    Added image upload endpoint

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-# main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import io
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "API is working"}
+    return {"message": "Image search backend is live"}
+
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))  # Optional image load
+    return {"filename": file.filename}
